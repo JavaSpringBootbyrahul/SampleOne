ebf5eaf762435029ed4d344ab368b01e029f0e6e
RAHUL-PC\Rzope <rzope5923@gmail.com>
2022-12-15 07:08:53 +0530

    Commit new changes To MYTest

--- MyTest.java
+++ MyTest.java
@@ -1,6 +1,6 @@
 public class MyTest {
     public static void main(String[] args) {
         System.out.println("Hello");
-        System.out.println("one");
+        System.out.println("Two");
     }
 }
