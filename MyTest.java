789bf42ed01281c1217a5e17c9b912bfdd2dccec
Rahul Zope <99934293+JavaSpringBootbyrahul@users.noreply.github.com>
2022-12-15 11:07:00 +0530

    Update MyTest.java

--- MyTest.java
+++ MyTest.java
@@ -1,6 +1,6 @@
 public class MyTest {
     public static void main(String[] args) {
         System.out.println("Hello");
-        System.out.println("Two");
+        System.out.println("One");
     }
 }
